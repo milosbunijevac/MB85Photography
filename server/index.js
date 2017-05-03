828f9f659253e72cea61039f0c696efe1a2e17e1
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-02 21:55:50 -0700

    "got the static files served"

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var path = require('path');
+
+var bodyParser = require('body-parser');
+
 var app = express();
 
+app.use(express.static('client'));
+
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, '../client/src', 'index.html'));
 });
 
 app.listen(3000);
