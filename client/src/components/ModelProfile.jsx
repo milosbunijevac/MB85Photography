2d7d040e7eb480c834b8545ffa04527453b329a3
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-24 17:38:06 -0700

    "starting to work on modelprofile"

---
+++ client/src/components/ModelProfile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ModelProfile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p> This is the model profile page. </p>
+      </div>
+    );
+  }
+}
+
+export default ModelProfile;
