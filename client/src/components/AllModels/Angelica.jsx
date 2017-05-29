1618a1cf230375ab75407b9e8cad3c4ac352e7df
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-05-29 14:07:42 -0700

    "added a specific route angelica to test"

---
+++ client/src/components/AllModels/Angelica.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Angelica extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        THIS IS ANGELICAS PAGE!
+      </div>
+    );
+  }
+}
+
+export default Angelica;
