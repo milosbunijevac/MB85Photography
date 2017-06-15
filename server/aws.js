3e06bffecbe5a4e661f550b28e7ec5ffe1afaa75
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-06-14 22:51:43 -0700

    without credential add

---
+++ server/aws.js
@@ -0,0 +1,62 @@
+var AWS = require('aws-sdk');
+var AWS = require('aws-sdk/global');
+var S3 = require('aws-sdk/clients/s3');
+var https = require('https');
+var agent = new https.Agent({
+  maxSockets: 25
+});
+
+
+AWS.config.region = 'us-west-2'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: 'us-west-2:0df8e51c-aa9e-4aa9-9b1c-937e2dc484a7',
+});
+AWS.config.apiVersions = {
+  dynamodb: '2011-12-05',
+  ec2: '2013-02-01',
+  redshift: 'latest'
+};
+AWS.config.update({
+  httpOptions: {
+    agent: agent
+  }
+});
+
+var s3 = new AWS.S3();
+var amazonLink = 'https://s3-us-west-2.amazonaws.com/mbimagestore/';
+var listparams = {
+  Bucket: 'mbimagestore', /* required */
+  EncodingType: 'url'
+};
+
+var listparams2 = {
+  Bucket: 'mbimagestore',
+  EncodingType: 'url'
+};
+
+let bucketNameLister = function(parameters, callback) {
+  s3.listObjects(parameters, function(err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+let descriptionGetter = function(parameters, callback) {
+  s3.getObject(parameters, function(err, data) {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data);
+    }
+  });
+};
+
+module.exports.s3 = s3;
+module.exports.bucketNameLister = bucketNameLister;
+module.exports.descriptionGetter = descriptionGetter;
+module.exports.listparams = listparams;
+module.exports.listparams2 = listparams2;
+module.exports.amazonLink = amazonLink;
