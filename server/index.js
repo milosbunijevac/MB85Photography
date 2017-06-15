e5516898f5cbf357109bcff0ad3f653fe3095e1a
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-06-14 23:36:51 -0700

    "made change to server index file"

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('client'));
+app.use(express.static('client/dist'));
 app.use(express.static('css'));
 app.use(express.static('fonts'));
 app.use(express.static('js'));
