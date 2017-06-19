b8cfc55c5be38578432fbcbf9b7f1b5f4c5389dc
Milos Bunijevac <milos.bunijevac@gmail.com>
2017-06-18 20:13:44 -0700

    "added about me clarity to text"

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -14,7 +14,7 @@ class About extends React.Component {
         </div>
 
         <div className = "col-sm-8">
-          <p>
+          <p className = "aboutText">
             My passion for photography started on a trip to San Francisco 3 years ago. Since then, I've worked with some of the top models in New York City 
             and have participated in three fashion weeks working runway shows. I shoot at various locations since I moved back to Cleveland, Ohio in November. 
             Feel free to contact me using the form below if you have any questions, requests or business inquiries. Thanks!
